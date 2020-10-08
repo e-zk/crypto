a54b8231b26220edd73fe0c058b69740d6fec1dd
zk <58356365+e-zk@users.noreply.github.com>
2020-10-08 23:33:39 +1000

    add CSV output flag to ecgraph

--- ecgraph/main.go
+++ ecgraph/main.go
@@ -8,24 +8,33 @@ import (
 
 func main() {
 
-        var p, a, b int
+        var str string  // store points lists as a string
+        var p, a, b int // variables
+        var csv bool    // csv?
 
         flag.IntVar(&p, "p", 11, "modulus")
         flag.IntVar(&a, "a", -2, "'a' value")
         flag.IntVar(&b, "b", 0, "'b' value")
+        flag.BoolVar(&csv, "csv", false, "output points as CSV at the end")
         flag.Parse()
 
-        str := "(inf)"
+        if !csv {
+                str = "(inf)"
+        }
+
         points := 1
 
         fmt.Printf("\nE: y^2 = x^3 + %dx + %d (mod %d)\n\n", a, b, p)
         for y := p - 1; y >= 0; y-- {
-                x := -1
-                for x = 0; x < p; x++ {
+                for x := 0; x < p; x++ {
                         x_cubed := int(math.Pow(float64(x), 3))
                         if ((p*p-y*y)+x_cubed+a*x+b)%p == 0 {
                                 points++
-                                str = fmt.Sprintf("(%d, %d) %s", x, y, str)
+                                if csv {
+                                        str = fmt.Sprintf("%d, %d\n%s", x, y, str)
+                                } else {
+                                        str = fmt.Sprintf("(%d, %d) %s", x, y, str)
+                                }
                                 print("@ ")
                         } else {
                                 print(". ")
@@ -33,5 +42,11 @@ func main() {
                 }
                 fmt.Printf("%d\n", y)
         }
+
+        // add an extra newline at the beginning if we're outputting csv
+        if csv {
+                str = fmt.Sprintf("\n%s", str)
+        }
+
         fmt.Printf("\nE= %s\n|E|= %d\n\n", str, points)
 }
